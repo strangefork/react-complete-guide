e6c672bb6d3acaa6eb1f4e5af1689683699a3919
Dan Eshleman <Dan.Eshleman@cdk.com>
2023-03-16 21:33:56 -0500

    Food App - 1
    Added some components for the food app. 
    I like how he uses the tree to plan out his components. I'm already looking forward to building GrindSmith.

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import classes from './AvailableMeals.module.css';
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -24,3 +26,14 @@ const DUMMY_MEALS = [
     price: 18.99,
   },
 ];
+
+const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map(meal => <li id={meal.id}>{meal.name}</li>);
+    return (
+        <section className={classes.meals}>
+            <ul>{mealsList}</ul>
+        </section>
+    )
+}
+
+export default AvailableMeals;
