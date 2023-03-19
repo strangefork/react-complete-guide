3e6f683554f5772e0a4ab86a7ee3af20fefc949d
Dan Eshleman <deshleman@fastmail.com>
2023-03-19 15:56:03 +0000

    Tried to fix the formatting for the MealItemForm component. Didn’t work.

--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,18 @@
-import classes from './MealItemForm.module.css';
-import Input from '../../UI/Input';
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
       <Input
-        label='Amount'
+        label="Amount"
         input={{
-          id: 'amount' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1',
+          id: "amount" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
