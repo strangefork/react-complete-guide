6a3b2853f3656ee39e315d959c9d2291cd5044ea
Dan Eshleman <Dan.Eshleman@cdk.com>
2023-05-02 22:03:10 -0500

    NextJS and Dynamic Page Generation
    My routes work but no data is loading for them...?

--- nextjs-bigger-project/pages/[meetupId]/index.js
+++ nextjs-bigger-project/pages/[meetupId]/index.js
@@ -1,14 +1,49 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-      title='A First Meetup'
-      address='5428 Yellowstone Dr.'
-      description='This is a first meetup.'
+      image={props.image}
+      title={props.title}
+      address={props.address}
+      description={props.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  //We have to pre-generate all the urls that could exist
+  return {
+    fallback: false, //False means we've outlined all possible paths. True means that it should try to generate it on the server.
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        title: 'A First Meetup',
+        address: '5428 Yellowstone Dr.',
+        description: 'This is a first meetup.',
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
--- nextjs-bigger-project/pages/index.js
+++ nextjs-bigger-project/pages/index.js
@@ -19,12 +19,39 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
+function HomePage(props) {
   return (
     <>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={props.meetups} />
     </>
   );
+}
+
+// export async function getServerSideProps(context) {
+//   //This will dynamically generate the page for each request.
+//   //This code only runs on the server.
+//   //This is slower than getStaticProps, because it can be cached and re-used. Should only use this if we need the context obj, or if the data changes multiple times every sencond.
+
+//   const req = context.request;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  //This function must be getStaticProps - and this is where we load data before the component function is executed.
+  //Any code you write in here will ONLY execute on the server, NOT in the browser. It happens during the build process.
+  //Fetch data from API/database, or access the file system. But then you return an object.
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS, //Data fetching being done during the build process. (I'm still not clear how the timing of this helps, but okay.)
+    },
+    revalidate: 1, //Incremental static generation. This is the number of sections NextJS will wait before it regenerates the page on the server.
+  };
 }
 
 export default HomePage;
